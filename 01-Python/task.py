9c2df16b5edb00026ac90f18d9938ca7861a778c
Manish Tanwar <0manishtanwar@gmail.com>
2023-07-23 22:44:22 +0530

    vault backup: 2023-07-23 22:44:22

--- 01-Python/task.py
+++ 01-Python/task.py
@@ -1,6 +1,27 @@
-# Question - 1
-# Input : "Data" "Science" "Mentorship" "Program" 
-# "By" "CampusX"
-# Output : "Data" "Science" "Mentorship" "Program" "By" "CampusX"
-print("Data" "Science" "Mentorship" "Program") 
-print("By" "CampusX")
+run = "ques1"
+
+
+"""
+### Q1 :- Print the given strings as per stated format.
+
+**Given strings**: 
+```
+"Data" "Science" "Mentorship" "Program" 
+"By" "CampusX"
+```
+**Output**: 
+``` 
+Data-Science-Mentorship-Program-started-By-CampusX
+```
+Concept- [Seperator and End]
+"""
+
+if run=="ques1":
+    print("Data","Science","Mentorship","Program",sep="-",end="-")
+    print("By",'CampusX',sep="-",)
+    
+
+
+
+if run=="ques2":
+    
