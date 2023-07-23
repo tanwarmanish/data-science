2c9e6c9b3410dc86add7f743584bbce3dff9929d
Manish Tanwar <0manishtanwar@gmail.com>
2023-07-23 22:35:39 +0530

    vault backup: 2023-07-23 22:35:39

---
+++ 01-Python/task.py
@@ -0,0 +1,6 @@
+# Question - 1
+# Input : "Data" "Science" "Mentorship" "Program" 
+# "By" "CampusX"
+# Output : "Data" "Science" "Mentorship" "Program" "By" "CampusX"
+print("Data" "Science" "Mentorship" "Program") 
+print("By" "CampusX")
